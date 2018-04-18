cea73bb995888014fa6bd694a9016e16598d6759
Johan Vilela <johanvilela@hotmail.com>
2018-04-18 15:31:13 -0300

    Add YOU ROCK

--- game.js
+++ game.js
@@ -73,5 +73,38 @@ function createBallons(count_balllons){
 function burst(balloon) {
     var balloon_id = balloon.id;
 
+    document.getElementById(balloon_id).setAttribute('onclick', '');
     document.getElementById(balloon_id).src = 'images/small_blue_burst_balloon.png';
+
+    update_score();
+}
+
+function update_score(){
+    var points = 1;
+
+    var full_balloons_score = document.getElementById('full_balloons').innerHTML;
+    var burst_balloons_score = document.getElementById('burst_balloons').innerHTML;
+
+    full_balloons_score = parseInt(full_balloons_score);
+    burst_balloons_score = parseInt(burst_balloons_score);
+
+    full_balloons_score -= points;
+    burst_balloons_score += points;
+
+    document.getElementById('full_balloons').innerHTML = full_balloons_score;
+    document.getElementById('burst_balloons').innerHTML = burst_balloons_score;
+
+    verify_game_status(full_balloons_score);
+    
+}
+
+function verify_game_status(full_balloons_score){
+    if(full_balloons_score == 0){
+        alert('YOU ROCK!');
+        stop_game();
+    }
+}
+
+function stop_game(){
+    clearTimeout(timerId);
 }
